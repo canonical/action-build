29a399a32e08626a788fb5cb8a4977117644c440
James Henstridge <james@jamesh.id.au>
2019-12-29 18:13:13 +0800

    src: add module with utilities that ensure build tools are available

---
+++ __tests__/tools.test.ts
@@ -0,0 +1,233 @@
+// -*- mode: javascript; js-indent-level: 2 -*-
+
+import * as exec from '@actions/exec'
+import fs = require('fs')
+import * as os from 'os'
+import * as path from 'path'
+import * as tools from '../src/tools'
+
+afterEach(() => {
+  jest.restoreAllMocks()
+})
+
+test('ensureSnapd installs snapd if needed', async () => {
+  expect.assertions(4)
+
+  const accessMock = jest.spyOn(fs.promises, 'access').mockImplementation(
+    async (filename: fs.PathLike, mode?: number | undefined): Promise<void> => {
+      throw new Error('not found')
+    }
+  )
+  const statMock = jest.spyOn(fs.promises, 'stat').mockImplementation(
+    async (filename: fs.PathLike): Promise<fs.Stats> => {
+      const s = new fs.Stats()
+      s.uid = 0
+      s.gid = 0
+      return s
+    }
+  )
+  const execMock = jest.spyOn(exec, 'exec').mockImplementation(
+    async (program: string, args?: string[]): Promise<number> => {
+      return 0
+    }
+  )
+
+  await tools.ensureSnapd()
+
+  expect(accessMock).toHaveBeenCalled()
+  expect(statMock).toHaveBeenCalled()
+  expect(execMock).toHaveBeenNthCalledWith(1, 'sudo', [
+    'apt-get',
+    'update',
+    '-q'
+  ])
+  expect(execMock).toHaveBeenNthCalledWith(2, 'sudo', [
+    'apt-get',
+    'install',
+    '-qy',
+    'snapd'
+  ])
+})
+
+test('ensureSnapd is a no-op if snapd is installed', async () => {
+  expect.assertions(3)
+
+  const accessMock = jest
+    .spyOn(fs.promises, 'access')
+    .mockImplementation(
+      async (
+        filename: fs.PathLike,
+        mode?: number | undefined
+      ): Promise<void> => {}
+    )
+  const statMock = jest.spyOn(fs.promises, 'stat').mockImplementation(
+    async (filename: fs.PathLike): Promise<fs.Stats> => {
+      const s = new fs.Stats()
+      s.uid = 0
+      s.gid = 0
+      return s
+    }
+  )
+  const execMock = jest.spyOn(exec, 'exec').mockImplementation(
+    async (program: string, args?: string[]): Promise<number> => {
+      return 0
+    }
+  )
+
+  await tools.ensureSnapd()
+
+  expect(accessMock).toHaveBeenCalled()
+  expect(statMock).toHaveBeenCalled()
+  expect(execMock).not.toHaveBeenCalled()
+})
+
+test('ensureSnapd fixes permissions on the root directory', async () => {
+  expect.assertions(3)
+
+  const accessMock = jest
+    .spyOn(fs.promises, 'access')
+    .mockImplementation(
+      async (
+        filename: fs.PathLike,
+        mode?: number | undefined
+      ): Promise<void> => {}
+    )
+  const statMock = jest.spyOn(fs.promises, 'stat').mockImplementation(
+    async (filename: fs.PathLike): Promise<fs.Stats> => {
+      const s = new fs.Stats()
+      s.uid = 500
+      s.gid = 0
+      return s
+    }
+  )
+  const execMock = jest.spyOn(exec, 'exec').mockImplementation(
+    async (program: string, args?: string[]): Promise<number> => {
+      return 0
+    }
+  )
+
+  await tools.ensureSnapd()
+
+  expect(accessMock).toHaveBeenCalled()
+  expect(statMock).toHaveBeenCalled()
+  expect(execMock).toHaveBeenCalledWith('sudo', ['chown', 'root:root', '/'])
+})
+
+test('ensureLXD installs the snap version of LXD if needed', async () => {
+  expect.assertions(3)
+
+  const accessMock = jest.spyOn(fs.promises, 'access').mockImplementation(
+    async (filename: fs.PathLike, mode?: number | undefined): Promise<void> => {
+      throw new Error('not found')
+    }
+  )
+  const execMock = jest.spyOn(exec, 'exec').mockImplementation(
+    async (program: string, args?: string[]): Promise<number> => {
+      return 0
+    }
+  )
+
+  await tools.ensureLXD()
+
+  expect(accessMock).toHaveBeenCalled()
+  expect(execMock).toHaveBeenNthCalledWith(1, 'sudo', [
+    'snap',
+    'install',
+    'lxd'
+  ])
+  expect(execMock).toHaveBeenNthCalledWith(2, 'sudo', ['lxd', 'init', '--auto'])
+})
+
+test('ensureLXD removes the apt version of LXD', async () => {
+  expect.assertions(2)
+
+  const accessMock = jest.spyOn(fs.promises, 'access').mockImplementation(
+    async (filename: fs.PathLike, mode?: number | undefined): Promise<void> => {
+      return
+    }
+  )
+  const execMock = jest.spyOn(exec, 'exec').mockImplementation(
+    async (program: string, args?: string[]): Promise<number> => {
+      return 0
+    }
+  )
+
+  await tools.ensureLXD()
+
+  expect(accessMock).toHaveBeenCalled()
+  expect(execMock).toHaveBeenNthCalledWith(1, 'sudo', [
+    'apt-get',
+    'remove',
+    '-qy',
+    'lxd',
+    'lxd-client'
+  ])
+})
+
+test('ensureLXD is a no-op if LXD is installed', async () => {
+  expect.assertions(2)
+
+  const accessMock = jest.spyOn(fs.promises, 'access').mockImplementation(
+    async (filename: fs.PathLike, mode?: number | undefined): Promise<void> => {
+      if (filename === '/snap/bin/lxd') {
+        return
+      }
+      throw new Error('not found')
+    }
+  )
+  const execMock = jest.spyOn(exec, 'exec').mockImplementation(
+    async (program: string, args?: string[]): Promise<number> => {
+      return 0
+    }
+  )
+
+  await tools.ensureLXD()
+
+  expect(accessMock).toHaveBeenCalled()
+  expect(execMock).not.toHaveBeenCalled()
+})
+
+test('ensureSnapcraft installs Snapcraft if needed', async () => {
+  expect.assertions(2)
+
+  const accessMock = jest.spyOn(fs.promises, 'access').mockImplementation(
+    async (filename: fs.PathLike, mode?: number | undefined): Promise<void> => {
+      throw new Error('not found')
+    }
+  )
+  const execMock = jest.spyOn(exec, 'exec').mockImplementation(
+    async (program: string, args?: string[]): Promise<number> => {
+      return 0
+    }
+  )
+
+  await tools.ensureSnapcraft()
+
+  expect(accessMock).toHaveBeenCalled()
+  expect(execMock).toHaveBeenNthCalledWith(1, 'sudo', [
+    'snap',
+    'install',
+    '--classic',
+    'snapcraft'
+  ])
+})
+
+test('ensureSnapcraft is a no-op if Snapcraft is installed', async () => {
+  expect.assertions(2)
+
+  const accessMock = jest.spyOn(fs.promises, 'access').mockImplementation(
+    async (filename: fs.PathLike, mode?: number | undefined): Promise<void> => {
+      return
+    }
+  )
+  const execMock = jest.spyOn(exec, 'exec').mockImplementation(
+    async (program: string, args?: string[]): Promise<number> => {
+      return 0
+    }
+  )
+
+  await tools.ensureSnapcraft()
+
+  expect(accessMock).toHaveBeenCalled()
+  expect(execMock).not.toHaveBeenCalled()
+})
---
+++ src/tools.ts
@@ -0,0 +1,53 @@
+// -*- mode: javascript; js-indent-level: 2 -*-
+
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+// Importing as an ECMAScript Module blocks access to fs.promises:
+//   https://github.com/nodejs/node/issues/21014
+import fs = require('fs') // eslint-disable-line @typescript-eslint/no-require-imports
+
+async function haveExecutable(path: string): Promise<boolean> {
+  try {
+    await fs.promises.access(path, fs.constants.X_OK)
+  } catch (err) {
+    return false
+  }
+  return true
+}
+
+export async function ensureSnapd(): Promise<void> {
+  const haveSnapd = await haveExecutable('/usr/bin/snap')
+  if (!haveSnapd) {
+    core.info('Installing snapd...')
+    await exec.exec('sudo', ['apt-get', 'update', '-q'])
+    await exec.exec('sudo', ['apt-get', 'install', '-qy', 'snapd'])
+  }
+  // The Github worker environment has weird permissions on the root,
+  // which trip up snap-confine.
+  const root = await fs.promises.stat('/')
+  if (root.uid !== 0 || root.gid !== 0) {
+    await exec.exec('sudo', ['chown', 'root:root', '/'])
+  }
+}
+
+export async function ensureLXD(): Promise<void> {
+  const haveDebLXD = await haveExecutable('/usr/bin/lxd')
+  if (haveDebLXD) {
+    core.info('Removing legacy .deb packaged LXD...')
+    await exec.exec('sudo', ['apt-get', 'remove', '-qy', 'lxd', 'lxd-client'])
+  }
+  const haveSnapLXD = await haveExecutable('/snap/bin/lxd')
+  if (!haveSnapLXD) {
+    core.info('Installing LXD...')
+    await exec.exec('sudo', ['snap', 'install', 'lxd'])
+    await exec.exec('sudo', ['lxd', 'init', '--auto'])
+  }
+}
+
+export async function ensureSnapcraft(): Promise<void> {
+  const haveSnapcraft = await haveExecutable('/snap/bin/snapcraft')
+  if (!haveSnapcraft) {
+    core.info('Installing Snapcraft...')
+    await exec.exec('sudo', ['snap', 'install', '--classic', 'snapcraft'])
+  }
+}
